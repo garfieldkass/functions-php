80f934c7ce54a913a92d116dd6c837251e98a2ad
Kalle Pungas <kallllle@gmail.com>
2016-02-26 23:22:37 +0200

    Create array-fn.php

---
+++ array-fn.php
@@ -0,0 +1,13 @@
+// array functions
+
+// array_reduce for strings
+// example usage: 
+// $options=array_reduce_str ( file($opt_list), function($s) { return "<option value='$s'>$s</option>\n"; } );
+function array_reduce_str($arr,$fn)
+{
+  $result='';
+  foreach ($arr as $item) $result.=$fn(trim($item));
+  return $result; 
+}
+
+
