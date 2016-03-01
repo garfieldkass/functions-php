ef9045482ec365026e4e464ba512e7de24d742e6
Kalle Pungas <kallllle@gmail.com>
2016-03-01 11:40:57 +0200

    Update array-fn.php

--- array-fn.php
+++ array-fn.php
@@ -1,6 +1,15 @@
 <?php
 // array functions
 
+
+
+// create (small) array from string, spending less characters in main code
+// ex: $array=arr('word1 word2 word3');
+function arr($s, $delimiter=' ')
+{
+  return explode($delimiter, $s);
+}
+
 // array_reduce for strings
 // example usage: 
 // $options=array_reduce_str ( file($opt_list), function($s) { return "<option value='$s'>$s</option>\n"; } );
